28044efec55049c4d30f4818e0f34e2271d46dfd
--system <mikejoyce19@gmail.com>
2016-10-10 20:54:41 -0500

    added logger

--- git-push.js
+++ git-push.js
@@ -1,12 +1,15 @@
 var ghpages = require('gh-pages')
 var path = require('path')
 
-var repo = 'test-branch'
+var repo = 'test-branch1'
 
 
 ghpages.publish(path.join(__dirname, 'dist'), {
     branch: repo,
-    message: `deploying to branch: ${repo}` 
+    message: `deploying to branch: ${repo}`,
+    logger: function(message) {
+        console.log(message)
+    }
 }, function (err) {
     if (err) {
         console.error('An error has occurred', err)
